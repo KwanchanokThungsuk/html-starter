dfa810eab7415b30452740b80dbcc5c095a1a242
KwanchanokThungsuk <kwanchanok0905196542@gmail.com>
2024-10-28 11:33:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
-        e.preventDefault();
+        e.preventDefault(); // ป้องกันการเลื่อนโดยตรง
 
         const targetId = this.getAttribute('href');
         const targetElement = document.querySelector(targetId);
         
+        // เลื่อนไปยังหัวข้อที่กำหนด
         targetElement.scrollIntoView({
-            behavior: 'smooth' 
+            behavior: 'smooth' // การเลื่อนอย่างเนียน
         });
     });
 });
@@ -16,7 +17,7 @@ function showImage() {
     var button = document.getElementById("toggleButton");
     
     if (!image.classList.contains("show")) {
-        image.classList.add("show"); 
-        button.innerHTML = none; 
+        image.classList.add("show"); // เพิ่มคลาส .show
+        button.innerHTML = none; // เปลี่ยนข้อความปุ่ม
     }
 }
